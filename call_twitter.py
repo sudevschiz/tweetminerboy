ddc023193cb74c0d127f9e3c377dcda6a36ef357
Sudev <sudevschiz@gmail.com>
2018-09-05 13:49:47 +0530

    Multiple options asked to the user for different types of tweet fetch

--- call_twitter.py
+++ call_twitter.py
@@ -9,6 +9,63 @@ if not os.path.exists(newpath):
 # searchList = ["journey mapping software", "customer experience", "journey mapping", "#CX", "#customerjourneymapping",
 #               "#CustomerJourney", "#CustomerExperience", "@forrester", "@IDC", "@HfSResearch", "@Mapovate", "@SuiteCX",
 #               "@TandemSeven", "@dtnpf", "@symantec", "@NortonOnline", "@PCGPPL"]
-searchList = ["@CMOKerala"]
-for item in searchList:
-    ts.fetch_tweets(item)
+searchList = ["@TryCatchExcept"]
+
+
+def option_one():
+    list_string = "\n".join(searchList)
+    print("Current fetch list is : \n" + list_string)
+    print("Starting fetch...\n")
+    for item in searchList:
+        ts.fetch_tweets(item)
+
+
+def option_two():
+    handles = input("Input the handle to be fetched. [Input multiple handles using , ]")
+    handle_list = handles.split(",")
+    print("Starting fetch...")
+
+    # TODO : Different fetching practise for tweets MADE BY a twitter handle
+    for item in handle_list:
+        print("In progress")
+        # ts.fetch_tweets(item)
+
+
+def option_three():
+    handles = input("Input the handle or hashtag to be streamed. [Input multiple handles using , ]")
+    handle_list = handles.split(",")
+    print("Starting fetch...")
+
+    # TODO : Twitter streaming
+    for item in handle_list:
+        print("In progress")
+        # ts.fetch_tweets(item)
+
+
+def option_four():
+    print("Exiting...")
+    return
+
+
+title = "\n\n=+=+ TWEET MINER +=+=\n\nChoose from options below : \n"
+
+print(title)
+
+print("[1] Fetch all mentions of a hashtag or a handle")
+print("[2] Fetch all tweets made by a handle")
+print("[3] Stream a hashtag")
+print("[4] Exit")
+
+choice = input('Enter your choice [1-4] : ')
+print(choice)
+choice = int(choice)
+
+print("Option " + str(choice) + " selected.")
+if choice == 1:
+    option_one()
+if choice == 2:
+    option_two()
+if choice == 3:
+    option_three()
+if choice == 4:
+    option_four()
--- twitter_scrape.py
+++ twitter_scrape.py
@@ -69,61 +69,68 @@ def fetch_tweets(searchquery):
                 ["id", "fetch_date", "created_at", "rt_status", "user_id", "user_name", "user_followers_count",
                  "user_favourites_count", "user_listed_count", "user_verified", "coordinates", "text"])
 
-            while tweetcount < maxtweets:
-                try:
-                    if max_id <= 0:
-                        if not sinceid:
-                            new_tweets = api.search(q=searchquery, tweet_mode="extended", count=tweetsperqry)
-                        else:
-                            new_tweets = api.search(q=searchquery, tweet_mode="extended", count=tweetsperqry,
-                                                    since_id=sinceid)
+        while tweetcount < maxtweets:
+            try:
+                if max_id <= 0:
+                    if not sinceid:
+                        new_tweets = api.search(q=searchquery, tweet_mode="extended", count=tweetsperqry)
                     else:
-                        if not sinceid:
-                            new_tweets = api.search(q=searchquery, tweet_mode="extended", count=tweetsperqry,
-                                                    max_id=str(max_id - 1))
-                        else:
-                            new_tweets = api.search(q=searchquery, tweet_mode="extended", count=tweetsperqry,
-                                                    max_id=str(max_id - 1),
-                                                    since_id=sinceid)
-                    if not new_tweets:
-                        print("No more tweets found")
-                        break
-
-                    for tweet in new_tweets:
-                        print(tweet)
-                        # print(tweet.full_text)
-                        all_data = json.loads(jsonpickle.encode(tweet._json, unpicklable=False))
-                        # all_data = json.loads(tweet)
-                        tweet_id = int(all_data["id_str"])
-                        # print(id)
-                        created_at = all_data["created_at"]
-                        # Add the coordinates
-
-                        if hasattr(tweet, 'retweeted_status'):
-                            rt_status = 1
-                            tweet_data = all_data["retweeted_status"]
-
-                        else:
-                            rt_status = 0
-                            tweet_data = all_data
-
-                        spec_attr = specific_attr(tweet_data)
-
-                        fetch_date = str(date.today())
-
-                        row_list = list()
-                        row_list.extend([tweet_id, fetch_date, created_at, rt_status])
-                        row_list.extend(spec_attr)
-                        # print(row_list)
-
-                        writer.writerow(row_list)
-
-                    tweetcount += len(new_tweets)
-                    print("Downloaded {0} tweets".format(tweetcount))
-                    max_id = new_tweets[-1].id
-                except tweepy.TweepError as e:
-                    # Just exit if any error
-                    print("some error : " + str(e))
+                        new_tweets = api.search(q=searchquery, tweet_mode="extended", count=tweetsperqry,
+                                                since_id=sinceid)
+                else:
+                    if not sinceid:
+                        new_tweets = api.search(q=searchquery, tweet_mode="extended", count=tweetsperqry,
+                                                max_id=str(max_id - 1))
+                    else:
+                        new_tweets = api.search(q=searchquery, tweet_mode="extended", count=tweetsperqry,
+                                                max_id=str(max_id - 1),
+                                                since_id=sinceid)
+                if not new_tweets:
+                    print("No more tweets found")
                     break
 
+                for tweet in new_tweets:
+                    print(tweet)
+                    # print(tweet.full_text)
+                    all_data = json.loads(jsonpickle.encode(tweet._json, unpicklable=False))
+                    # all_data = json.loads(tweet)
+                    tweet_id = int(all_data["id_str"])
+                    # print(id)
+                    created_at = all_data["created_at"]
+                    # Add the coordinates
+
+                    if hasattr(tweet, 'retweeted_status'):
+                        rt_status = 1
+                        tweet_data = all_data["retweeted_status"]
+
+                    else:
+                        rt_status = 0
+                        tweet_data = all_data
+
+                    spec_attr = specific_attr(tweet_data)
+
+                    fetch_date = str(date.today())
+
+                    row_list = list()
+                    row_list.extend([tweet_id, fetch_date, created_at, rt_status])
+                    row_list.extend(spec_attr)
+                    # print(row_list)
+
+                    writer.writerow(row_list)
+
+                tweetcount += len(new_tweets)
+                print("Downloaded {0} tweets".format(tweetcount))
+                max_id = new_tweets[-1].id
+            except tweepy.TweepError as e:
+                # Just exit if any error
+                print("some error : " + str(e))
+                break
+
     print("Downloaded all tweets")
+
+
+def fetch_scheduler(time_interval_sec, fetch_max_count=1000):
+    fetch_counter = 0
+
+    while fetch_counter < fetch_max_count:
+        print("I am here")
